392709ec444626050bc4f4a030ea10df22a89e7a
matheuspeixeiro <65266315+matheuspeixeiro@users.noreply.github.com>
2021-06-25 08:18:10 -0300

    tirei o codigo

--- calculadora_determinante_3x3.cpp
+++ calculadora_determinante_3x3.cpp
@@ -1,46 +1,45 @@
 /***	CALCULADORA DE DETERMINANTE DE MATRIZ QUADRADA 3X3
 		E/OU	 DE PRODUTO MISTO.
-	Nome: Matheus Simões Peixeiro		Código: 835733
-   Curso: Engenharia da Computação		 Etapa: 1ª 	  ***/
+	Nome: Matheus SimÃµes Peixeiro			  ***/
 
 #include<stdio.h>
 #include<stdlib.h>
 #include<locale.h>
 
 main(){
-	// variáveis
+	// variÃ¡veis
 	setlocale(LC_ALL,"Portuguese");
 	int m[3][3], i, j, versor_i, versor_j, versor_k, determinante;
 	
 	//entrada de dados
 	for(i=0; i<3; i++){
 		for (j=0; j<3; j++){
-			printf("Bem-vindo à calculadora de determinante de matriz quadrada 3x3:\n\n");
+			printf("Bem-vindo Ã  calculadora de determinante de matriz quadrada 3x3:\n\n");
 			printf("Informe o valor para [%d][%d] : ", i+1, j+1);
 			scanf("%d", &m[i][j]);
 			system("cls");
 		}
 	}
 	
-	// processamento de dados - cálculo dos versores i,j,k e o determinante
+	// processamento de dados - cÃ¡lculo dos versores i,j,k e o determinante
 	versor_i = (m[1][1] * m[2][2] - (m[1][2] * m[2][1]))*m[0][0];
 	versor_j = (m[1][0] * m[2][2] - (m[1][2] * m[2][0]))*m[0][1];
 	versor_k = (m[1][0] * m[2][1] - (m[1][1] * m[2][0]))*m[0][2];
 	
 	determinante = versor_i - versor_j + versor_k;
 	
-	// saída de dados
+	// saÃ­da de dados
 	printf("\n");
 	for(i=0; i<3; i++){
 		printf("| ");
-		/* if-else para melhorar visualização } se positivo => 1 espaço
+		/* if-else para melhorar visualizaÃ§Ã£o } se positivo => 1 espaÃ§o
 			para alinhar com o numero negativo */
 		if(m[i][j] > 0) 
 			printf(" %d\t", m[i][j]);
 		else
 			printf("%d\t", m[i][j]);
 		for(j=1; j<3; j++){
-			/* if-else para melhorar visualização } se positivo => 1 espaço
+			/* if-else para melhorar visualizaÃ§Ã£o } se positivo => 1 espaÃ§o
 					para alinhar com o numero negativo */
 				if(m[i][j] > 0) 
 					printf(" %d\t", m[i][j]);
